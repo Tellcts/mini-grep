ad86816650c87301987c92045a0d78b19ce6add9
Tzcan <tzcan@foxmail.com>
2026-04-09 09:14:27 +0800

    Relayout the directories

--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,64 @@ impl Config {
         Ok(Config { query, filename })
     }
 }
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut res: Vec<&str> = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            res.push(line);
+        }
+    }
+    res
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut res = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(query) {
+            res.push(line);
+        }
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    //     #[test]
+    //     fn case_insensitive() {
+    //         let query = "rUsT";
+    //         let contents = "\
+    // Rust:
+    // safe, fast, productive.
+    // Pick three.
+    // Trust me.";
+
+    //         assert_eq!(
+    //             vec!["Rust:", "Trust me."],
+    //             search_case_insensitive(query, contents)
+    //         );
+    //     }
 }
