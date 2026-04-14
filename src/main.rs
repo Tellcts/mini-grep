4f370adc79be14a6edcc941a924950d0c38c943d
Tzcan <tzcan@foxmail.com>
2026-04-14 14:54:05 +0800

    Minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use minigrep::Config;
 use std::env;
 use std::process;
+use tzcan::Config;
 
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|err| {
@@ -8,7 +8,7 @@ fn main() {
         process::exit(1)
     });
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = tzcan::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     };
